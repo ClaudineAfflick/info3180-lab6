924ac498eeec838859bad4b4581a94377980268e
ClaudineAfflick <claudine.afflick@mymona.uwi.edu>
2019-03-25 02:27:01 +0000

    part1

--- app/static/js/app.js
+++ app/static/js/app.js
@@ -43,7 +43,12 @@ Vue.component('app-footer', {
 Vue.component('news-list', {
     template: `
        <div class="news">
-            <li v-for="article in articles" class="news__item">{{ article.title }}</li>
+            <h2>News</h2>
+            <ul class="news__list">
+            <li class="news__item">News item 1</li>
+            <li class="news__item">News item 2</li>
+            <li class="news__item">News item 3</li>
+            </ul>
         </div>
     `,
     created: function() {
