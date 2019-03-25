824f06dd13a46c776a0794cea00c107a3a5183db
ClaudineAfflick <claudine.afflick@mymona.uwi.edu>
2019-03-25 03:56:42 +0000

    final

--- app/static/js/app.js
+++ app/static/js/app.js
@@ -11,10 +11,10 @@ Vue.component('app-header', {
               <div class="collapse navbar-collapse" id="navbarSupportedContent">
                 <ul class="navbar-nav mr-auto">
                   <li class="nav-item active">
-                    <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+                    <router-link to="/" class="nav-link">Home</router-link>
                   </li>
                   <li class="nav-item">
-                    <a class="nav-link" href="#">News</a>
+                    <router-link to="/news" class="nav-link">News</router-link>
                   </li>
                 </ul>
               </div>
@@ -37,10 +37,10 @@ Vue.component('app-footer', {
             year: (new Date).getFullYear()
         }
     }
-})
+});
 
 
-Vue.component('news-list', {
+const NewsList= Vue.component('news-list', {
     template: `
        <div class="news">
             <h2>News</h2>
@@ -48,7 +48,7 @@ Vue.component('news-list', {
                 <div class="form-group mx-sm-3 mb-2">
                     <label class="sr-only" for="search">Search</label>
                     <input type="search" name="search" v-model="searchTerm" id="search" class="form-control mb-2 mr-sm-2" placeholder="Enter search term here" />
-                    <p>You are searching for {{ searchTerm }}</p>
+                    <button class="btn btn-primary mb-2" @click="searchNews">Search</button>
                 </div>
             </div>
             <ul class="news__list">
@@ -57,7 +57,7 @@ Vue.component('news-list', {
         </div>
     `,
     created: function() {
-        fetch('https://newsapi.org/v2/top-headlines? country=us&apiKey=<bea606c7a4c649228d756cfc2e9dbf58>')
+        fetch('https://newsapi.org/v2/top-headlines? country=us&apiKey=<>')
         .then(function(response) {
             return response.json();
             
@@ -84,7 +84,7 @@ Vue.component('news-list', {
     methods: {
         searchNews: function() {
             let self = this;
-            fetch('https://newsapi.org/v2/everything?q='+self.searchTerm + '&language=en&apiKey=<bea606c7a4c649228d756cfc2e9dbf58>')
+            fetch('https://newsapi.org/v2/everything?q='+self.searchTerm + '&language=en&apiKey=<>')
             .then(function(response) {
                 return response.json();
                 
@@ -101,11 +101,33 @@ Vue.component('news-list', {
     
 });
 
-
-let app = new Vue({
-    el: '#app',
-    data: {
-        welcome: 'Hello World! Welcome to VueJS'
-    }
+const Home = Vue.component('home', {
+ template: `
+ <div class="home">
+ <img src="/static/images/logo.png" alt="VueJS Logo">
+ <h1>{{ welcome }}</h1>
+ </div>
+ `,
+ data: function() {
+ return {
+ welcome: 'Hello World! Welcome to VueJS'
+ }
+ }
 });
+
+const router = new VueRouter({
+ mode: 'history',
+ routes: [
+ { path: '/', component: Home },
+ { path: '/news', component: NewsList }
+ ]
+});
+
+
+const app = new Vue({
+ el: '#app',
+ router
+});
+
+
 
